58d262187257f8593cba84399be8aa8a34ce18c7
Sayan Biswas <143019848+Sayan139@users.noreply.github.com>
2024-12-08 11:24:56 +0530

    update on countnum

--- basic/math/countnum.cpp
+++ basic/math/countnum.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int countnum(int n){
     if(n==0) return 0;
     int count =0;
-    while(n!=0){
+    while(n!=0){//here what we are doing is we are dividing the num by 10 and incrementing the value of count on each iteration 
         count++;
         n/=10;
     }
     return count;
-}
+}// this is the method yoou can solve the problem on how to find the num of digits the given num have
 int main(){
     int n;
     cout << "enter the value of the number: ";
