d2819d80b05a417e3c4ceb6da83ce9ea6dc5bfc5
Sayan Biswas <143019848+Sayan139@users.noreply.github.com>
2024-12-13 23:55:47 +0530

    potd problems

---
+++ potd/day1.py
@@ -0,0 +1,5 @@
+#A sorted array of distinct elements arr[] is rotated at some unknown point, the task is to find the minimum element in it
+def findMin(self, arr):
+        #complete the function here
+        largest = min(arr)
+        return largest
