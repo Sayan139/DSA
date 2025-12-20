0b74e31924136cb3306978afc9421bb65e2c6287
Sayan Biswas <sayanbiswas@Sayans-MacBook-Air.local>
2025-12-20 23:31:49 +0530

    first commit

---
+++ Maths & Combinations/odd_and_even.java
@@ -0,0 +1,13 @@
+public class odd_and_even {
+    static void fun(int n){
+        if(n%2 == 0){
+            System.out.println("Even number");
+        }
+        else{
+            System.out.println("Odd number ");
+        }
+    }
+    public static void main(String[] args) {
+        fun(3);
+    }
+}
